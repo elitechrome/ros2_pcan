f6545204f923dcb5e1c923a1fa99bcc05c8b181c
Lee Jaemin <ljm6525@gmail.com>
2018-12-11 23:07:58 +0900

    can_msgs/Frame 추가

--- src/mac_pcan.cpp
+++ src/mac_pcan.cpp
@@ -1,6 +1,7 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "can_msgs/msg/frame.hpp"
 
 using namespace std::chrono_literals;
 
@@ -13,7 +14,7 @@ public:
   MacPCANNode()
   : Node("mac_pcan"), count_(0)
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic");
+    publisher_ = this->create_publisher<can_msgs::msg::Frame>("topic");
     timer_ = this->create_wall_timer(
       500ms, std::bind(&MacPCANNode::timer_callback, this));
   }
@@ -21,13 +22,13 @@ public:
 private:
   void timer_callback()
   {
-    auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    auto message = can_msgs::msg::Frame();
+    message.data[0] = 0x01;
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%x'", message.data[0]);
     publisher_->publish(message);
   }
   rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Publisher<can_msgs::msg::Frame>::SharedPtr publisher_;
   size_t count_;
 };
 
